2e8fa74de32b4dfb1acc7c3110fed0937439bef5
khalil mohammad <you@example.comkhalilovske13@gmail.com>
2019-01-29 14:19:20 +0200

    changes

---
+++ Class works/29.1.2019/29.1.2019/Source.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+/*Написати функцію, яка приймає три аргументи: години, хвилини, секунди.
+Функція повинна повертати еквівалент переданого їй тимчасового значення в секундах */
+
+int Time(int Hour, int Minute){
+
+	 return  (Hour * 60) + (Minute * 60);
+}
+
+int main()
+{
+	int Hour = 0,  Minute = 0,  Result = 0;
+	cout << "ENTER YOUR HOUR AND MINUTE : " << endl;
+	cin >> Hour >> Minute;
+	Result= Time(Hour, Minute);
+	cout << "THE RESULT IS :" << Result << endl;
+	system("pause");
+	return 0;
+}
+
+
+
+
+
