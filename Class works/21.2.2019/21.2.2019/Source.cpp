b097ca82d1f5ee1b6a515d6485af0a3b8483bad6
mohammad khalil <khalil@step.itstep.rv.ua>
2019-02-21 14:21:15 +0200

    New and Delete memory + Tasks

---
+++ Class works/21.2.2019/21.2.2019/Source.cpp
@@ -0,0 +1,103 @@
+﻿/*Дано два масива : .Утворити третій масив мінімального можливого розміру, у якому зібрати :
+Елементи обох масивів;
+Спільні элементи обох массивов;
+Елементи масиву A, які не зустрічаються у B;
+Елементи масиву В, які не зустрічаються у А;
+Елементи масивів A і B, які не є спільними для них(тобто об’єднання результатів двох попереднії результатів).
+*/
+
+
+/*int *p= new int;
+	cout << "*p=" << *p << endl;
+	cout<<"p=" << p << endl;
+	*/
+//#include<iostream>
+//#include<ctime>
+//using namespace std;
+//
+//template<typename T>
+//void FillArr(T arr[], const int SIZE);
+//
+//
+//template<typename T>
+//void Print(T arr[], const int SIZE);
+//
+//
+//template<typename T>
+//void Mix(T arr[], T arr1[], T Arr[], const int SIZE, const int size);
+//
+//
+//
+//int main()
+//{
+//	srand(unsigned(time(NULL)));
+//	const int size = 10;
+//	const int SIZE = 5;
+//
+//	int arr[SIZE];
+//	int arr1[SIZE];
+//	int Arr[size];
+//
+//
+//	FillArr(arr, SIZE);
+//	FillArr(arr1, SIZE);
+//	Print(arr, SIZE);
+//	Print(arr1, SIZE);
+//    Mix( arr,arr1,Arr,SIZE,size);
+//	Print( Arr, size);
+//
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//template<typename T>
+//void Print(T arr[], const int SIZE)
+//{
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		cout << " arr [ " << i << " ] = " << arr[i] << endl;
+//	}
+//	cout << "----------------------------------->" << endl;
+//}
+//
+//template<typename T>
+//void FillArr(T arr[], const int SIZE)
+//{
+//	for (int i = 0; i < SIZE; i++) {
+//		arr[i] = rand() % 10 ;
+//	}
+//}
+//
+//template<typename T>
+//void Mix(T arr[],T arr1[],T Arr[], const int SIZE, const int size)
+//{
+//	int tmp = 0;
+//	for (int i = 0; i < size; i++) {
+//		if (i <= 4) {
+//			Arr[i] = arr[i];
+//		}
+//		else {
+//			Arr[i] = arr1[tmp];
+//			tmp++;
+//		}
+//	}
+//}
+
+//---------------------------------TASK 2 -------------------------------
+/*Створити 3 динамічних змінних різного типу.Заповнити їх деякими значеннями.
+Обчислити і вивести на екран їх добуток, а також самі значення динамічних змінних*/
+
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+	srand(unsigned(time(NULL)));
+	int 
+
+
+    system("pause");
+	return 0;
+}
