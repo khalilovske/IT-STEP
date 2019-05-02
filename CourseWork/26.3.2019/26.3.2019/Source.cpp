0bffbec2fefa5956ede3e0eb266b6c4b9cf78f75
khalil mohammad <you@example.comkhalilovske13@gmail.com>
2019-05-02 15:23:05 +0300

    Writing and Reading Library

--- CourseWork/26.3.2019/26.3.2019/Source.cpp
+++ CourseWork/26.3.2019/26.3.2019/Source.cpp
@@ -13,16 +13,92 @@
 //	Для обробки даних скористатися динамічним масивом покажчиків на структури відповідного типу.
 
 #include<iostream>
-#include<ctime>
 #include<string>
+#include<fstream>
 
 using namespace std;
-
-int main()
+void WriteFile(string Book, string Magazine, string Newspaper, string Author, int Year)
 {
+	ofstream WriteFile;
+	string path = "Library.txt";
+	WriteFile.open(path, ofstream::app);
+	if (!WriteFile.is_open()) {
+		cout << " Cann't open this file " << endl;
+	}
+	else
+	{
+		WriteFile << Book << "\t" << Magazine << "\t" << Newspaper << "\t" << Author << "\t" << Year << endl;
+	}
+	WriteFile.close();
+	cout << "==============================." << endl;
+}
 
+void ReadFile() {
+	ifstream ReadFile;
+	string path = "Library.txt";
+	ReadFile.open(path);
+	if (!ReadFile.is_open()) {
+		cout << " Cann't Read this file " << endl;
+	}
+	else {
+		cout << " Reading  " << endl;
+		string data;
+		while (!ReadFile.eof()) {
+			getline(ReadFile, data);
+			cout << data << endl;
+		}
+	}
+	ReadFile.close();
+	cout << "==============================." << endl;
+}
+
+void Creat_Library(string Book, string Magazine, string Newspaper, string Author, int Year)
+{
+	cout << "Enter your Book's Name :" << endl;
+	cin >> Book;
+	cout << "Enter your Magazine's Name :" << endl;
+	cin >> Magazine;
+	cout << "Enter your Newspaper's Name :" << endl;
+	cin >> Newspaper;
+	cout << "Enter your Author's Name :" << endl;
+	cin >> Author;
+	cout << "Enter the Production's Year :" << endl;
+	cin >> Year;
+	cout << "Enter the Production's Year :" << endl;
+	WriteFile(Book, Magazine, Newspaper, Author, Year);
+}
+
+void Menu(string Book, string Magazine, string Newspaper, string Author, int Year) {
+	int Number = 0;
+	cout << "Enter the Number of operration you want : \n 1.Creat Library\n2/Read Library's Files \n" << endl;
+	cin >> Number;
+	if (Number == 1) {
+		Creat_Library(Book, Magazine, Newspaper, Author, Year);
+	}
+	else if (Number == 2) {
+		ReadFile();
+	}
+	else {
+		cout << "Unavailable " << endl;
+	}
+}
+
+int Year;
+int main()
+{	
+	string Book;
+	string Magazine;
+	string Newspaper;
+	string Author;
+	
+
+	Menu(Book,Magazine,Newspaper,Author,Year);
+	Creat_Library(Book,Magazine,Newspaper,Author,Year);
+	ReadFile();
 
 
 	system("pause");
 	return 0;
 }
+
+
